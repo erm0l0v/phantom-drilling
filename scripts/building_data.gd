991f053581f8da4441ae527da2317e4c78487146
Kirill Ermolov <erm0l0v@ya.ru>
2026-07-18 16:10:53 +0200

    change settings

--- scripts/building_data.gd
+++ scripts/building_data.gd
@@ -2,8 +2,12 @@ class_name BuildingData
 
 enum BuildingType { COAL_PLANT, OXYGEN_FACTORY, STEAM_ENGINE }
 
-const TEXTURES := {
-	BuildingType.COAL_PLANT: preload("res://assets/coal_plant-Sheet.png"),
-	BuildingType.OXYGEN_FACTORY: preload("res://assets/oxigen_factory-Sheet.png"),
-	BuildingType.STEAM_ENGINE: preload("res://assets/steam_engine-Sheet.png"),
-}
+static var TEXTURES: Dictionary
+
+
+static func _static_init() -> void:
+	TEXTURES = {
+		BuildingType.COAL_PLANT: load("res://assets/coal_plant-Sheet.png"),
+		BuildingType.OXYGEN_FACTORY: load("res://assets/oxigen_factory-Sheet.png"),
+		BuildingType.STEAM_ENGINE: load("res://assets/steam_engine-Sheet.png"),
+	}
--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -1,18 +1,22 @@
 extends Node
 
-const PieceScene := preload("res://scenes/piece.tscn")
 const STAGING_ANCHOR := Vector2(480, 96)
 
 var current_piece: Piece = null
+var _piece_scene: PackedScene
 
 @onready var pieces_container: Node2D = get_node("../PiecesContainer")
+
+
+func _ready() -> void:
+	_piece_scene = load("res://scenes/piece.tscn")
 
 
 func spawn_next() -> void:
 	var type: TetrominoData.ShapeType = randi() % TetrominoData.ShapeType.size()
 	var building_type: BuildingData.BuildingType = randi() % BuildingData.BuildingType.size()
 	var rotation: int = randi() % 4
-	var piece: Piece = PieceScene.instantiate()
+	var piece: Piece = _piece_scene.instantiate()
 	pieces_container.add_child(piece)
 	piece.setup(type, STAGING_ANCHOR, building_type, rotation)
 	piece.placed.connect(_on_piece_placed)
